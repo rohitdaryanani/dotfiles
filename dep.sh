01eb337df515dd959f806fb6c29f7ff8a0f4326a
Elizar Pepino <jupenz@gmail.com>
2013-10-08 04:18:26 +0800

    updated dep.sh added powerline and jade plugin in janus folder

--- dep.sh
+++ dep.sh
@@ -8,3 +8,9 @@ chmod +x ~/.bin/z/z.sh
 # janus vim
 cd ~/
 curl -Lo- https://bit.ly/janus-bootstrap | bash
+# add powerline
+mkdir -p ~/.janus
+cd ~/.janus
+git clone https://github.com/Lokaltog/vim-powerline.git
+# vim-jade
+git clone git://github.com/digitaltoad/vim-jade.git
